ffb4ff44dcce38866cfed2cc841d246eb1940cba
melgert <mdelgert@yahoo.com>
2022-09-22 17:59:50 -0400

    Working EF notes example

--- BlazorEF.Server/Program.cs
+++ BlazorEF.Server/Program.cs
@@ -1,13 +1,18 @@
 using BlazorEF.Server.Data;
+using BlazorEF.Shared;
+using BlazorEF.Shared.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<DataContext>();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddTransient<INoteService, NoteService>();
 
 var app = builder.Build();
 
---
+++ BlazorEF.Shared/StringExt.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorEF.Shared;
+
+public static class StringExt
+{
+    public static string? Truncate(this string? value, int maxLength, string truncationSuffix = "…")
+    {
+        return value?.Length > maxLength
+            ? value.Substring(0, maxLength) + truncationSuffix
+            : value;
+    }
+}
+
+//https://stackoverflow.com/questions/2776673/how-do-i-truncate-a-net-string
