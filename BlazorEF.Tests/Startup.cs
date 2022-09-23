f48321c9b17176d8c03e2a37769d46abc9c1ea03
melgert <mdelgert@yahoo.com>
2022-09-23 01:49:02 -0400

    update startup

--- BlazorEF.Tests/Startup.cs
+++ BlazorEF.Tests/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorEF.Shared;
 using BlazorEF.Shared.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorEF.Tests;
 
@@ -9,7 +10,13 @@ public class Startup
     {
         //var dataContext = new DataContext();
         //services.AddSingleton(_ => dataContext);
+
+        services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(
+                "Data Source=localhost;Initial Catalog=BlazorEF;Integrated Security=False;Persist Security Info=False;User ID=sa;Password=Password2022;"));
+        
         services.AddSingleton<INoteService, NoteService>();
+        
         services.AddLogging(loggerBuilder =>
         {
             loggerBuilder.ClearProviders();
