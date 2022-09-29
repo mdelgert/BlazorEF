7b43156c57ec12c5eaf2152a21b63a56d95f2b3f
melgert <mdelgert@yahoo.com>
2022-09-29 14:50:30 -0400

    Add database project

---
+++ BlazorEF.Database/dbo/Tables/Notes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [NoteId]      NVARCHAR (450) NOT NULL,
+    [Title]       NVARCHAR (MAX) NULL,
+    [Message]     NVARCHAR (MAX) NULL,
+    [CreateDate]  DATETIME2 (7)  NOT NULL,
+    [UpdatedDate] DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED ([NoteId] ASC)
+);
+
